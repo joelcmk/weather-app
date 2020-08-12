f4451ee5663a01f17181af0f421e67c7a4f59d02
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-12 17:46:41 -0500

    added apiKey and created weekContainer component

---
+++ src/weekContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WeekContainer extends Component {
+  render() {
+    return (
+      <div className="week-container">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default WeekContainer;
