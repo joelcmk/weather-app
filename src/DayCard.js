c9d31af222b2aefb4268235109d2de2dedc6866b
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-12 18:36:59 -0500

    Added DayCard component and funtion to fetch info form API

---
+++ src/DayCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DayCard extends Component {
+  render() {
+    return (
+      <div className="dayCard">
+        <p>hi</p>
+      </div>
+    )
+  }
+}
+
+export default DayCard;
