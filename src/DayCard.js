c10fe9495bc75affe6e4cf7a0c98a5444580305e
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-13 20:44:12 -0500

    added icons

--- src/DayCard.js
+++ src/DayCard.js
@@ -1,23 +1,25 @@
 import React from 'react';
-var moment = require('moment')
+import './owfont-master/css/owfont-regular.css'
+var moment = require('moment');
 
-const DayCard = ({ reading }) => {
 
-  const newDate = new Date();
+const DayCard = ({ reading, degreeType }) => {
+  let newDate = new Date();
   const weekday = reading.dt * 1000
-  newDate.setTime(weekday);
+  newDate.setTime(weekday)
+
+  const fahrenheit = Math.round(reading.main.temp)
+  const celsius = Math.round((fahrenheit - 32) * 5 / 9)
 
   const imgURL = `owf owf-${reading.weather[0].id} owf-5x`
 
   return (
     <div className="col-sm-2">
       <div className="card">
-        <h3 className="card-title">{moment(newDate).format(
-          'ddd'
-        )}</h3>
-        <p>{moment(newDate).format('MMM Do, h:mm a')}</p>
+        <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+        <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
         <i className={imgURL}></i>
-        <h2>{Math.round(reading.main.temp)} °F</h2>
+        <h2>{degreeType === "celsius" ? celsius + "°C" : fahrenheit + "°F"}</h2>
         <div className="card-body">
           <p className="card-text">{reading.weather[0].description}</p>
         </div>
--- src/weekContainer.js
+++ src/weekContainer.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import apiConfig from './apiKeys';
-import DayCard from './DayCard'
+import DayCard from './DayCard';
+import DegreeToggle from './DegreeToggle';
+
+const weatherURL =
+  `http://api.openweathermap.org/data/2.5/forecast?zip=78741&units=imperial&APPID=${apiConfig.owmKey}`
 
 class WeekContainer extends Component {
 
   state = {
     fullData: [],
     dailyData: [],
+    degreeType: "farenheit"
   }
 
   componentDidMount = () => {
-    const weatherURL =
-      `http://api.openweathermap.org/data/2.5/forecast?zip=78741&units=imperial&APPID=${apiConfig.owmKey}`
-
     fetch(weatherURL)
       .then(res => res.json())
       .then(data => {
@@ -24,8 +26,14 @@ class WeekContainer extends Component {
       })
   }
 
+  updateForecastDegree = event => {
+    this.setState({
+      degreeType: event.target.value
+    }, () => console.log(this.state))
+  }
+
   formatDayCards = () => {
-    return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+    return this.state.dailyData.map((reading, index) => <DayCard degreeType={this.state.degreeType} reading={reading} key={index} />)
   }
 
   render() {
@@ -33,6 +41,7 @@ class WeekContainer extends Component {
       <div className="week-container">
         <h1 className="display-1 jumbotron">5-Day Forecast</h1>
         <h5 className="display-5 text-muted">Austin, US</h5>
+        <DegreeToggle degreeType={this.state.degreeType} updateForecastDegree={this.updateForecastDegree} />
         <div className="row justify-content-center">
           {this.formatDayCards()}
         </div>
